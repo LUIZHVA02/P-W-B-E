2d95024b234e9a7403c3f58d86c9c60aa34082ff
LUIZHVA <luizhva02@gmail.com>
2023-07-28 17:26:41 -0300

    28/07/2023

---
+++ aula-1/app.js
@@ -0,0 +1,17 @@
+//Comentário em linha.
+
+/* 
+    Comentário 
+    em Bloco.
+*/
+
+console.log("Testando o Node.JS");
+
+var nome = "Luiz Vidal";
+
+console.log("O nome do usuário é: " + nome);
+
+console.log(`O Nome do Usuário é: ${nome}`);
+
+//typeof() - Permite identificar o tipo de dados  de uma variável ou de um objeto.
+console.log(typeof(nome));
