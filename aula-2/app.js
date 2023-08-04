6b18eac59560aa2a4b1b0ea131a2aed77ae92651
LUIZHVA <luizhva02@gmail.com>
2023-08-04 17:28:54 -0300

    04/08/2023

--- aula-2/app.js
+++ aula-2/app.js
@@ -14,26 +14,23 @@ var entradaDeDados = readline.createInterface({
     output:process.stdout
 })
 
+console.log(``)
 //Solicita a entrada de dados no terminal, através de uma função de CallBack do método question
 entradaDeDados.question('Digite seu nome: ', function(nomeUsuario){
     //Recebe na variavel nome o conteúdo digitado pelo usuário
     var nome = nomeUsuario
 
-    //Exibe na tela um texto e a variável
-    
-
     entradaDeDados.question('Digite sua idade: ', function(idadeUsuario){
         var idade = idadeUsuario
-        
 
         entradaDeDados.question('Digite seu telefone: ', function(telefoneUsuario){
             var telefone = telefoneUsuario
-            
 
             entradaDeDados.question('Digite seu email: ', function(emailUsuario){
                 var email = emailUsuario
                 console.log(``)
-                console.log(`Bem Vindo ao Aplicativo ${nome}`)
+                //Exibe na tela um texto e a variável
+                console.log(`Bem Vindo ao Aplicativo,  ${nome}`)
                 console.log(`A idade do usuário ${nome} é: ${idade}`)
                 console.log(`O telefone do usuário ${nome} é: ${telefone}`)
                 console.log(`O email do usuário ${nome} é: ${email}`)
