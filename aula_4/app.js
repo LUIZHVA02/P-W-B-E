38f094b6645c0906b7d2dacfa8b7f5567d1a0d42
LUIZHVA <luizhva02@gmail.com>
2023-08-18 17:24:17 -0300

    18/08/2023

--- aula_4/app.js
+++ aula_4/app.js
@@ -1,50 +1,65 @@
 var readline = require('readline')
 
+//Cria objeto para realizar as entradas de dados
 var entradaDeDados = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
 console.log('')
 
-entradaDeDados.question('Digite o primeiro número: ', function(primeiroNumero){
-    let numero1 = Number (primeiroNumero)
+//Entrada de dados do primeiro valor
+entradaDeDados.question('Digite o primeiro número: ', function (primeiroNumero) {
+    
+    let numero1 = primeiroNumero.replace(',','.')
 
-    entradaDeDados.question('Digite o segundo número: ', function(segundoNumero){
-        let numero2 = Number (segundoNumero)
-            console.log('')
-            
-        
-            if(isNaN(numero1)||isNaN(numero2)){
-                console.log('ERRO. DIGITE APENAS NÚMEROS!')}
-                else{
-                entradaDeDados.question('As operações disponiveis são: \nAdição(+) Subtração(-) Divisão(/) Multiplicação(x)\nDigite a operação desejada: ', function(resposta){
-                    var operacao = resposta
-                   
-                    if(operacao =='+'){
-                        let resultado = Number(numero1) + Number(numero2)
-                        console.log(`O resultado de ${numero1} + ${numero2} é: ${resultado}`)
-    
-                    } else if(operacao=='-'){
-                        resultado = Number(numero1) - Number(numero2)
-                        console.log(`O resultado de ${numero1} - ${numero2} é: ${resultado}`)
-    
-                    } else if(operacao=='/'){
-                        resultado == Number(numero1) / Number(numero2)
-                        console.log(`O resultado de ${numero1} / ${numero2} é: ${resultado}`)
-    
-                    } else if(operacao=='x'){
-                        resultado = Number(numero1) * Number(numero2)
-                        console.log(`O resultado de ${numero1} X ${numero2} é: ${resultado}`)
+    //Entrada de dados do segundo valor
+    entradaDeDados.question('Digite o segundo número: ', function (segundoNumero) {
+        let numero2 = segundoNumero.replace(',','.')
 
-                    } else{
-                        console.log('ERRO. POR FAVOR DIGITE UMA RESPOSTA VÁLIDA')
-                    }
-            
-            })
-        
+        console.log('')
+
+
+        if (isNaN(numero1) || isNaN(numero2) || numero1 == '' || numero2 == '') {
+            console.log('ERRO. DIGITE APENAS NÚMEROS!')
         }
-            
+        else {
+            //Entrada de dados referente a operação matemática a ser realizada
+            entradaDeDados.question('As operações disponiveis são: \nAdição(+) Subtração(-) Divisão(/) Multiplicação(x)\nDigite a operação desejada: ', function (resposta) {
+                var operacao = resposta
+
+                numero1 = Number(numero1)
+                numero2 = Number(numero2)
+
+                if (operacao == '+') {
+                    let resultado = numero1 + numero2
+                    console.log(`O resultado de ${numero1} + ${numero2} é: ${resultado}`)
+
+                } else if (operacao == '-') {
+                    resultado = numero1 - numero2
+                    console.log(`O resultado de ${numero1} - ${numero2} é: ${resultado}`)
+
+                } else if (operacao == '/') {
+
+                    if(numero2 == 0){
+                        console.log('ERRO. Não é possível dividir um número por 0')
+                    } else {
+                        resultado = numero1 / numero2
+                        console.log(`O resultado de ${numero1} / ${numero2} é: ${resultado}`)
+                    }
+
+                } else if (operacao == 'x' || operacao == 'X') {
+                    resultado = numero1 * numero2
+                    console.log(`O resultado de ${numero1} X ${numero2} é: ${resultado}`)
+
+                } else {
+                    console.log('ERRO. POR FAVOR DIGITE UMA RESPOSTA VÁLIDA')
+                }
+
+            })
+
+        }
+
     })
 
 })
