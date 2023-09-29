344eddd8d21c32def5ff344ea2b3e1bebd757498
Luiz Henrique Vidal Araújo <luizhva02@gmail.com>
2023-09-29 08:48:45 -0300

    29/09/2023

--- aula_7/lista_de_exercicios/exercicio02/app.js
+++ aula_7/lista_de_exercicios/exercicio02/app.js
@@ -7,23 +7,23 @@ var entradaDeDados = readline.createInterface({
     output: process.stdout
 });
 
-entradaDeDados.question('Digite o nome do aluno(a): ', function(nomeAluno){
-    let aluno = nomeAluno
+entradaDeDados.question('Digite o nome do aluno(a): ', function(aluno){
+    let nomeAluno = aluno
 
-    entradaDeDados.question('Digite o nome do professor(a): ', function(nomeDoProfessor){
-        let professor = nomeDoProfessor
+    entradaDeDados.question('Digite o nome do professor(a): ', function(professor){
+        let nomeDoProfessor = professor
         
-        entradaDeDados.question('Digite o gênero do aluno(a): ', function(generoAluno){
+        entradaDeDados.question('Digite o gênero do aluno(a)(M)Masculino (F)Feminino: ', function(generoAluno){
             let sexoAluno = generoAluno
         
-            entradaDeDados.question('Digite o gênero do professor(a): ', function(generoProfessor){
+            entradaDeDados.question('Digite o gênero do professor(a)(M)Masculino (F)Feminino: ', function(generoProfessor){
                 let sexoProfessor = generoProfessor
                 
-                entradaDeDados.question('Digite o nome do curso: ', function(nomeCurso){
-                    let curso = nomeCurso
+                entradaDeDados.question('Digite o nome do curso: ', function(curso){
+                    let nomeCurso = curso
                 
-                    entradaDeDados.question('Digite o nome da disciplina: ', function(nomeDisciplina){
-                        let disciplina = nomeDisciplina
+                    entradaDeDados.question('Digite o nome da disciplina: ', function(disciplina){
+                        let nomeDisciplina = disciplina
                         
                         entradaDeDados.question('Digite a primeira nota: ', function(valorNota1){
                             let nota1 = Number (valorNota1)
@@ -36,10 +36,28 @@ entradaDeDados.question('Digite o nome do aluno(a): ', function(nomeAluno){
             
                                     entradaDeDados.question('Digite a segunda nota: ',function(valorNota4){
                                         let nota4 = Number (valorNota4)
-                                        
-                                        mediasEscolares.distribuirDados (nomeAluno, sexoAluno, nomeDoProfessor, 
+
+                                            console.log('')
+                                            console.log('/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/')
+                                            console.log('')
+                                            
+                                        mediasEscolares.mediaComum (nomeAluno, sexoAluno, nomeDoProfessor, 
                                                                         sexoProfessor, nomeCurso, nomeDisciplina,
-                                                                        nota1, nota2, nota3, nota4, notaExame)
+                                                                        nota1, nota2, nota3, nota4)
+
+                                            console.log('')
+                                            console.log('/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/')
+                                            console.log('')
+                                        
+                                        if(media >= 50 && media < 69){
+
+                                            entradaDeDados.question('Digite a nota do exame: ', function(exame){
+                                                let notaExame = Number (exame)
+                                        
+                                                mediasEscolares.notaDoExame (notaExame)
+                                            
+                                            })                                    
+                                        }
                                     })
                                 })
                             })
--- aula_7/lista_de_exercicios/exercicio02/modulo/mediasEscolares.js
+++ aula_7/lista_de_exercicios/exercicio02/modulo/mediasEscolares.js
@@ -1,8 +1,9 @@
-const distribuirDados = function 
+const mediaComum = function 
     (nomeAluno, sexoAluno, nomeDoProfessor, 
     sexoProfessor, nomeCurso, nomeDisciplina,
-    nota1, nota2, nota3, nota4, notaExame) {
-    
+    nota1, nota2, nota3, nota4) {
+
+
     let studentsName = nomeAluno
     let teachersName = nomeDoProfessor
     let studentsGender = sexoAluno
@@ -13,42 +14,30 @@ const distribuirDados = function
     let grade2 = Number (nota2)
     let grade3 = Number (nota3)
     let grade4 = Number (nota4)
-    let examsGrade = Number (notaExame)
     let media = (grade1+grade2+grade3+grade4)/4
-    let media2 = (average + examsGrade)/2
-    let situacao
+    let situacao = ''
 
     if (nota1 < 0 || nota1 > 100 || nota2 < 0 || nota2 > 100 || 
-        nota3 < 0 || nota3 > 100 || nota4 < 0 || nota4 > 100 ||
-        notaExame < 0 || notaExame > 100){
+        nota3 < 0 || nota3 > 100 || nota4 < 0 || nota4 > 100 ){
 
         console.log('Erro. O Valor Inserido é Inválido!!!')
 
     } else if (studentsGender == '' || studentsName == '' || teachersGender == '' || 
                 teachersName == '' || coursesName == '' || className == '' || 
-                grade1 == '' || grade2 == '' || grade3 == '' || grade4 == '' || 
-                examsGrade == '' || average){
+                grade1 == '' || grade2 == '' || grade3 == '' || grade4 == ''){
 
                 console.log('Erro. Nenhum Valor Inserido!!!')
     }
 
-    if(media > 70){
+    if(media >= 70){
         situacao = 'aprovado'
 
-    } else if(media < 50){
+    } if(media < 50){
         situacao = 'reprovado'
 
-    } else if(media > 50 && media < 69){
-        
-        if(media2 < 60){
-            situacao = 'reprovado'
-
-        } else if (media2>=60) {
-            situacao = 'aprovado em exame'    
-        }
     }
 
-    if(studentsGender == 'Masculino' && teachersGender == 'Masculino'){
+    if(studentsGender == 'M' && teachersGender == 'M'){
         console.log(`O Aluno ${studentsName} foi ${situacao} na Disciplina ${className}`)
         console.log(`Curso: ${coursesName}`)
         console.log(`Professor: ${teachersName}`)
@@ -56,7 +45,7 @@ const distribuirDados = function
         console.log(`Média Final: ${media}`)
         console.log(`Média Final do Exame: ${media2}`)
 
-    } else if(studentsGender == 'Feminino' && teachersGender == 'Masculino'){
+    } else if(studentsGender == 'F' && teachersGender == 'M'){
 
         console.log(`A Aluna ${studentsName} foi ${situacao} na Disciplina ${className}`)
         console.log(`Curso: ${coursesName}`)
@@ -65,7 +54,7 @@ const distribuirDados = function
         console.log(`Média Final: ${media}`)
         console.log(`Média Final do Exame: ${media2}`)
 
-    } else if(studentsGender == 'Masculino' && teachersGender == 'Feminino'){
+    } else if(studentsGender == 'M' && teachersGender == 'F'){
         
         console.log(`O Aluno ${studentsName} foi ${situacao} na Disciplina ${className}`)
         console.log(`Curso: ${coursesName}`)
@@ -74,7 +63,7 @@ const distribuirDados = function
         console.log(`Média Final: ${media}`)
         console.log(`Média Final do Exame: ${media2}`)
 
-    } else if(studentsGender == 'Feminino' && teachersGender == 'Feminino'){
+    } else if(studentsGender == 'F' && teachersGender == 'F'){
         
         console.log(`A Aluna ${studentsName} foi ${situacao} na Disciplina ${className}`)
         console.log(`Curso: ${coursesName}`)
@@ -83,9 +72,77 @@ const distribuirDados = function
         console.log(`Média Final: ${media}`)
         console.log(`Média Final do Exame: ${media2}`)
     }
+    return mediaComum
 }
 
 module.exports = {
-    distribuirDados
+    mediaComum
 }
 
+const notaDoExame = (nomeAluno, sexoAluno, nomeDoProfessor, 
+                    sexoProfessor, nomeCurso, nomeDisciplina,
+                    nota1, nota2, nota3, nota4,notaExame) =>{
+
+    let studentsName = nomeAluno
+    let teachersName = nomeDoProfessor
+    let studentsGender = sexoAluno
+    let teachersGender = sexoProfessor
+    let coursesName = nomeCurso
+    let className = nomeDisciplina
+    let grade1 = Number (nota1)
+    let grade2 = Number (nota2)
+    let grade3 = Number (nota3)
+    let grade4 = Number (nota4)
+    let media = (grade1+grade2+grade3+grade4)/4
+    let situacao = ''
+    let media2 = (media + examsGrade)/2
+
+            if(media2 < 60){
+            situacao = 'reprovado'
+
+            } else if (media2 >= 60) {
+            situacao = 'aprovado em exame'    
+            }
+
+            if(studentsGender == 'M' && teachersGender == 'M'){
+                console.log(`O Aluno ${studentsName} foi ${situacao} na Disciplina ${className}`)
+                console.log(`Curso: ${coursesName}`)
+                console.log(`Professor: ${teachersName}`)
+                console.log(`Notas do Aluno: ${grade1}, ${grade2}, ${grade3}, ${grade4}, ${examsGrade}`)
+                console.log(`Média Final: ${media}`)
+                console.log(`Média Final do Exame: ${media2}`)
+        
+            } else if(studentsGender == 'F' && teachersGender == 'M'){
+        
+                console.log(`A Aluna ${studentsName} foi ${situacao} na Disciplina ${className}`)
+                console.log(`Curso: ${coursesName}`)
+                console.log(`Professor: ${teachersName}`)
+                console.log(`Notas da Aluna: ${grade1}, ${grade2}, ${grade3}, ${grade4}, ${examsGrade}`)
+                console.log(`Média Final: ${media}`)
+                console.log(`Média Final do Exame: ${media2}`)
+        
+            } else if(studentsGender == 'M' && teachersGender == 'F'){
+                
+                console.log(`O Aluno ${studentsName} foi ${situacao} na Disciplina ${className}`)
+                console.log(`Curso: ${coursesName}`)
+                console.log(`Professora: ${teachersName}`)
+                console.log(`Notas do Aluno: ${grade1}, ${grade2}, ${grade3}, ${grade4}, ${examsGrade}`)
+                console.log(`Média Final: ${media}`)
+                console.log(`Média Final do Exame: ${media2}`)
+        
+            } else if(studentsGender == 'F' && teachersGender == 'F'){
+                
+                console.log(`A Aluna ${studentsName} foi ${situacao} na Disciplina ${className}`)
+                console.log(`Curso: ${coursesName}`)
+                console.log(`Professora: ${teachersName}`)
+                console.log(`Notas da Aluna: ${grade1}, ${grade2}, ${grade3}, ${grade4}, ${examsGrade}`)
+                console.log(`Média Final: ${media}`)
+                console.log(`Média Final do Exame: ${media2}`)
+            }
+        
+        
+    }
+
+module.exports = {
+    notaDoExame
+}
