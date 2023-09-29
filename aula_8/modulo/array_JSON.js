18b62f922e63e779e80cb454df2b7ad7e2aac28e
LUIZHVA <luizhva02@gmail.com>
2023-09-29 11:22:23 -0300

    introducao

--- aula_8/modulo/array_JSON.js
+++ aula_8/modulo/array_JSON.js
@@ -24,7 +24,68 @@ const introducaoAoArray = function () {
     //Adiciona elementos novos no final do Array
     listaDeNomes.push('Roberto')
 
+    // Adiciona elementos novos no inicio da lista
+    listaDeNomes.unshift('Oliveira')
+    console.table(listaDeNomes)
+
+    // Remove o ultimo elemento do Array
+    listaDeNomes.pop()
+    console.table(listaDeNomes)
+
+    // Remove o primeiro elemento do Array
+    listaDeNomes.shift()
+    console.table(listaDeNomes)
+
+    //Remove um elemento baseado no índice, podendo escolher a quantidade de itens a serem excluídos
+    //Exemplo: splice(2,3) -> 2 é refente ao índice e 3 -> é refente a quantidade de itens
+    listaDeNomes.splice(2,3)
+
+    //Mostra o valor e o indice do Array
+    listaDeNomes.forEach(function (item, indice) {
+        console.log(item, indice);
+    });
+
+    //Adiciona elementos com tipos de dados diferentes
+    listaDeNomes.push('André da Silva', 'Ricardo da Silva', 'joão da Silva', 10, true)
+    console.log(listaDeNomes)
+
+    // Typeof mostra o tipo de dados de um elemento
+    console.log(typeof(listaDeNomes))
+    console.log(typeof(listaDeNomes[5]))
+    console.log(typeof(listaDeNomes[6]))
+
 
 }
 
-introducaoAoArray()
+const percorrendoArray = function () {
+
+    let listaDeProdutos = ['Mouse', 'Teclado', 'Monitor', 'Impressora','Mouse Pad', 'Gabinete', 'Processador', 'HD']
+
+    // let cont = 0
+
+    // while(cont < listaDeProdutos.length){
+    //     console.log(`Nome do Produto: ${listaDeProdutos[cont]}`)
+    //     cont++
+    // }
+
+    // for(let cont = 0; cont < listaDeProdutos.length; cont++){
+    //     console.log(`Nome do Produto: ${listaDeProdutos[cont]}`)
+    // }
+
+    // ForEach
+    // listaDeProdutos.forEach(function(produto, indice){
+    //     console.log((indice+1) + ' - Nome do Produto:' + produto)
+    // })
+
+    // indexOf Retorna o indice do elemento que foi encontrado
+    // Caso o item buscado não exista, retorna sempre -1
+    console.log(listaDeProdutos.indexOf('Impressora'))
+
+    //Retorna verdadeiro ou falso
+    console.log(listaDeProdutos.includes('Mouse'))
+
+}
+
+percorrendoArray()
+
+//introducaoAoArray()
