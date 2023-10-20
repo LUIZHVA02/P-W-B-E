95f3213f6506f913cfe5a31c17957e6a3e174461
LUIZHVA <luizhva02@gmail.com>
2023-10-20 09:38:07 -0300

    20/10/2023

---
+++ aula_010/manipulandoArrayEJSON.js
@@ -0,0 +1,61 @@
+const getListaDeEstados = function () {
+/**********************************************
+*
+● Criar uma função (getListaDeEstados) que 
+retorna a lista de todos os estados do Brasil. 
+*
+**********************************************/
+
+
+}
+
+const getDadosEstado = function (){
+/**********************************************
+*
+● Criar uma função (getDadosEstado) que retorna 
+as informações referente a um estado do Brasil, 
+onde a sigla do estado será o critério de filtro. 
+*
+**********************************************/
+}
+
+const getCapitalEstado = function (){
+/**********************************************
+*
+● Criar uma função (getCapitalEstado) que retorna 
+as informações referente a capital de um estado do 
+Brasil, onde a sigla do estado será o critério de
+filtro. 
+*
+**********************************************/
+}
+
+const getEstadosRegiao = function () {
+/**********************************************
+*
+● Criar uma função (getEstadosRegiao) que retorna 
+as informações referente aos estados do Brasil 
+conforme a sua região, onde a região será o
+critério de filtro.
+*
+**********************************************/
+}
+
+const getCapitalPais = function(){
+/**********************************************
+*
+● Criar uma função (getCapitalPais) que retorna 
+as informações referente aos estados que formam 
+a capital do Brasil. 
+*
+**********************************************/
+}
+
+const getCidades = function (){
+/**********************************************
+*
+● Criar uma função (getCidades) que retorna uma 
+lista de cidades, filtrado pela sigla do estado.  
+*
+**********************************************/
+}
